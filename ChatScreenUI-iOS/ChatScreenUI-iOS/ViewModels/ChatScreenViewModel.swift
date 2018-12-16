cf89d6eb51af51da74bf809c5819c40e26fcddca
macneko-ayu <macneko.ayu@gmail.com>
2018-12-17 00:28:28 +0900

    ChatScreenViewModelを追加

---
+++ ChatScreenUI-iOS/ChatScreenUI-iOS/ViewModels/ChatScreenViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ChatScreenViewModel.swift
+//  ChatScreenUI-iOS
+//
+//  Created by 横田孝次郎 on 2018/12/14.
+//  Copyright © 2018年 macneko. All rights reserved.
+//
+
+import Foundation
+
+struct ChatScreenViewModel {
+
+}
