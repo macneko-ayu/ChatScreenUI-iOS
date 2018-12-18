811b9e88644835c73693ec5b8c51c78b78c28bbc
Kojiro Yokota <macneko.ayu@gmail.com>
2018-12-18 11:15:21 +0900

    UserIconを追加、実装

---
+++ ChatScreenUI-iOS/ChatScreenUI-iOS/Models/UserIcon.swift
@@ -0,0 +1,17 @@
+//
+//  UserIcon.swift
+//  ChatScreenUI-iOS
+//
+//  Created by Kojiro Yokota on 2018/12/18.
+//  Copyright © 2018年 macneko. All rights reserved.
+//
+
+import Foundation
+
+struct UserIcon {
+    var initials: String
+
+    init(initials: String = "?") {
+        self.initials = initials
+    }
+}
