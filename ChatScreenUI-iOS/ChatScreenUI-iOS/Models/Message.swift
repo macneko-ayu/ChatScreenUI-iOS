a7f9ddf04a8ccbbb7b6577ae7ecad0412aaf3d91
Kojiro Yokota <macneko.ayu@gmail.com>
2018-12-18 11:11:11 +0900

    Modelを実装

--- ChatScreenUI-iOS/ChatScreenUI-iOS/Models/Message.swift
+++ ChatScreenUI-iOS/ChatScreenUI-iOS/Models/Message.swift
@@ -8,6 +8,26 @@
 
 import Foundation
 
+enum MessageKind {
+    case text(String)
+}
+
 struct Message {
-    
+    let id: String
+    let user: User
+    let createdAt: Date
+    let kind: MessageKind
+
+    init(id: String, user: User, createdAt: Date, kind: MessageKind) {
+        self.id = id
+        self.user = user
+        self.createdAt = createdAt
+        self.kind = kind
+    }
+}
+
+extension Message: Equatable {
+    static func == (lhs: Message, rhs: Message) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
--- ChatScreenUI-iOS/ChatScreenUI-iOS/Models/User.swift
+++ ChatScreenUI-iOS/ChatScreenUI-iOS/Models/User.swift
@@ -6,6 +6,14 @@
 //  Copyright © 2018年 macneko. All rights reserved.
 //
 
-struct User {
+import Foundation
 
+struct User: Equatable {
+    let id: String
+    let name: String
+
+    init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
